6e66f23ba9721a200394eeee1b2f36705ef95639
Eacker <daniel.eacker@dfg.alaska.local>
2022-04-12 17:22:10 -0800

    Minor update to parameter description in update_model().

--- R/localSCR.R
+++ R/localSCR.R
@@ -2746,12 +2746,12 @@ realized_density <- function(samples, grid, crs_, site, hab_mask, s_alias = "s",
 #' Allows for efficient editing of model code produced by \code{nimbleCode()} function
 #'
 #' @param model \code{nimbleCode()} used to define model in \code{nimble} package,
-#' possibly generated from \code{\link{get_classic}}.
+#' possibly generated from \code{\link{get_classic}} function.
 #' @param line_remove either \code{NULL} or a integer value defining which lines
 #' of code to remove from model. Set to \code{NULL} when only appending to and
 #' not replacing code in prevous model file.
 #' @param append_code either \code{NULL} or model code produced from 
-#' \code{nimbleCode} function or \code{\link{get_classic}}. Note that if
+#' \code{nimbleCode()}  or \code{\link{get_classic}} function. Note that if
 #' \code{line_remove = NULL}, then code will be appended just after existing
 #' model code; otherwise specify the lines to replace when appending by setting
 #' \code{line_remove}.
