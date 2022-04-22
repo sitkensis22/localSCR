9046ea6e5951cab7060a79e9a2577402b66150c6
Eacker <daniel.eacker@dfg.alaska.local>
2022-04-22 10:18:52 -0800

    Updated customize_model

--- R/localSCR.R
+++ R/localSCR.R
@@ -2993,19 +2993,34 @@ customize_model <- function(model,append_code = NULL,append_line = NULL,
    if(length(append_line) != length(append_model[-c(1,length(append_model))])){
      stop(paste("length of append_line must equal lines of append_code minus outside braces, i.e.,","{","}"))
    }
-   if(is.null(remove_line)){
+    if(is.null(remove_line)){
         updated_model <- character(length(main_model)+length(append_model)-2)
         updated_model[append_line] <- append_model[-c(1,length(append_model))]
         updated_model[(1:(length(updated_model)-1))[-c(1,append_line)]] <- main_model[-c(1,length(main_model))]
         updated_model[c(1,length(updated_model))] <- c("nimble::nimbleCode({","})") 
    }else
    if(isFALSE(is.null(remove_line))){ 
-        updated_model <- character(length(main_model))
-        updated_model[remove_line] <- NA
-        updated_model[append_line] <- append_model[-c(1,length(append_model))]
-        updated_model[c(1,length(updated_model))] <- c("nimble::nimbleCode({","})") 
-        updated_model[which(updated_model == "")] <- remove_model[-c(1,length(remove_model))]
-        updated_model <- updated_model[is.na(updated_model)==FALSE]
+        update_model <- character(length(main_model))
+        update_model[(1:length(main_model))[-remove_line]] <- remove_model
+        # if max of append_line equal to length model
+        if(max(append_line) == length(main_model)){
+        update_model[length(update_model)] <- ""
+        update_model <- c(update_model,"})")
+        update_model[append_line] <- append_model[-c(1,length(append_model))]
+        update_model <- update_model[which(update_model!="")]
+        }else
+        # if max of append_line equal to length model
+        if(max(append_line) < length(main_model)){
+        update_model[append_line] <- append_model[-c(1,length(append_model))]
+        update_model <- update_model[which(update_model!="")]
+        }else
+        # if max of append_line greater to length model
+        if(max(append_line) > length(main_model)){
+        update_model <- c(update_model[-length(update_model)],
+                          rep("",max(append_line)-length(main_model)+1),"})")
+        update_model[append_line] <- append_model[-c(1,length(append_model))]
+        update_model <- update_model[which(update_model!="")] 
+        }
    } # check for line removal
   } # end append_code and append_line = TRUE
     # if nothing is done to code, just return original code
